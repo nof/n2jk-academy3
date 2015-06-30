daa8dc6d34b8684bf016a75fdb69a2a3539625e0
nof <sakape@gmail.com>
2015-06-30 13:00:59 +0900

    localeの警告

--- config/application.rb
+++ config/application.rb
@@ -39,6 +39,7 @@ module N2jkAcademy3
 
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
+    I18n.enforce_available_locales = false
     config.i18n.default_locale = :ja
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
