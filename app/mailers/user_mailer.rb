cb1460615a183addd47f46ef07a7d60cc4acd93a
nof <sakape@gmail.com>
2015-06-30 14:47:36 +0900

    post時のエラー

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -2,7 +2,9 @@ class UserMailer < ApplicationMailer
 
   def post_created(post)
     @post = post
-    mail(to: User.where.not(user: @post.user).map(&:email), subject: "[n2jk-academy] #{@post.user.name}が週報を投稿しました")
+    if tos = User.where.not(id: @post.user.id).map(&:email).presence
+      mail(to: tos, subject: "[n2jk-academy] #{@post.user.name}が週報を投稿しました")
+    end
   end
 
   def comment_created(comment)
