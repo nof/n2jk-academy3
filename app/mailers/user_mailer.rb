2b1cea133d4fb7059e2304160e02622060d2d5a7
nof <sakape@gmail.com>
2015-06-30 14:20:06 +0900

    Commentのメール

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -2,7 +2,13 @@ class UserMailer < ApplicationMailer
 
   def post_created(post)
     @post = post
-    mail(to: User.all.map(&:email), subject: "[n2jk-academy] #{@post.user.name}が週報を投稿しました")
+    mail(to: User.where.not(user: @post.user).map(&:email), subject: "[n2jk-academy] #{@post.user.name}が週報を投稿しました")
+  end
+
+  def comment_created(comment)
+    @comment = comment
+    return if @comment.user == @comment.post.user
+    mail(to: @comment.post.user.email, subject: "[n2jk-academy] #{@comment.user.name}がコメントしました")
   end
 
 end
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,6 +2,11 @@ class Comment < ActiveRecord::Base
   belongs_to :user
   belongs_to :post
   acts_as_readable on: :created_at
+  after_create :deliver
 
   validates :body, presence: true
+
+  def deliver
+    UserMailer.comment_created(self).deliver_later!
+  end
 end
